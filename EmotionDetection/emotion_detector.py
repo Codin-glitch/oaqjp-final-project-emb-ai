d2d36867099b1c8353eeaa2a41b24da73880ddf8
Codin_glitch <pratishtha1607@gmail.com>
2024-07-25 05:48:41 -0400

    task_7

--- EmotionDetection/emotion_detector.py
+++ EmotionDetection/emotion_detector.py
@@ -7,12 +7,31 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers = header)
     formatted_response = json.loads(response.text)
-    d = dict(formatted_response['emotionPredictions'][0]['emotion'])
 
-    max_score = 0
-    for key in d:
-        if d[key]>max_score:
-            max_score=d[key]
-            dominant_emotion = key
-    d['dominant_emotion'] = dominant_emotion
-    return d
+    if not text_to_analyse.strip():
+        return {
+        "anger": None,
+        "disgust": None,
+        "fear": None,
+        "joy": None,
+        "sadness": None, 
+        "dominant_emotion":None
+        }
+    
+    elif response.status_code == 200:
+        d = dict(formatted_response['emotionPredictions'][0]['emotion'])
+
+        max_score = 0
+        for key in d:
+            if d[key]>max_score:
+                max_score=d[key]
+                dominant_emotion = key
+        d['dominant_emotion'] = dominant_emotion
+        return d
+    elif response.status_code == 400:
+        d = dict(formatted_response['emotionPredictions'][0]['emotion'])
+        for key in d.keys():
+            d[key] = None
+        d['dominant_amotion'] = None
+        return d
+    
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
+'''Importing necessary modules'''
+
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detector import emotion_detector
-from flask import Flask,request,render_template
 
 app = Flask('Emotion Detector')
 
 @app.route('/emotionDetector')
 def emo_detector():
+    ''' This function will analyze the text given by users and give the emotion score '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     anger = response['anger']
@@ -13,10 +16,15 @@ def emo_detector():
     joy = response['joy']
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    if dominant_emotion is None:
+        return "Invalid test! Please try again!."
+    return f"For the given statement, the system response is \
+    'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, \
+    'joy': {joy}, 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
 @app.route('/')
 def render_index_page():
+    '''To render the home page'''
     return render_template('index.html')
 
 if __name__=='__main__':
